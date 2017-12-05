95f3f77c6e1c3d870437da0c774082067f84e219
Sergey Sherstobitov <in43sh@gmail.com>
2017-12-04 17:57:55 -0700

    Question 3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      arr: ['spaghetti', 'ice cream', 'sushi', 'bologna', 'cheese'],
+      userInput: ''
+    }
+  }
+
+  handleChange(val) {
+    this.setState({ userInput: val })
+  }
+
   render() {
+    let displayArray = this.state.arr.filter((el) => {
+      return el.includes(this.state.userInput)
+    }).map((el, i) => {
+      return (
+        <h2 key={ i }>{ el }</h2>
+      )
+    } )
+
+    // let displayArray = this.state.arr.filter((el) => el.startsWith(this.state.userInput)).map((el, i) => {
+    //   return (
+    //     <h2 key={ i }>{ el }</h2>
+    //   )
+    // } )
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={(e) => this.handleChange(e.target.value)}></input>
+        {displayArray}
       </div>
     );
   }
