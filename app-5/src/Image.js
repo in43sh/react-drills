f2ade8469a9874c4d94434aa205e734642dd492f
Sergey Sherstobitov <in43sh@gmail.com>
2017-12-04 19:45:04 -0700

    initial commit

---
+++ app-5/src/Image.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Image = () => {
+  return (<img src="https://http.cat/402" alt="" />);
+}
+
+//  function Image (props) {
+//   return (
+//     <div>
+//       <img src={ props.myImage } alt="" />
+//     </div>
+//   );
+// }
+
+export default Image
